5d4df6dd5467ad2947842f688e9a03eee3de29bd
Sarah Khalil <mkhalil.sarah@gmail.com>
2017-01-11 11:32:57 +0100

    Added creation and updated of an article
    Added redirection for homepage and added  creation of article
    
    Completed the form to add an article
    
    Finished add and update of an article

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -2,13 +2,19 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Article;
+use AppBundle\Form\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @Route("/article")
+ */
 class ArticleController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="article_homepage")
      */
     public function homepageAction()
     {
@@ -20,11 +26,62 @@ class ArticleController extends Controller
      *     "/{id}",
      *     requirements={"id" = "\d+"},
      *     defaults={"id" = 1},
-     *     name="show_article"
+     *     name="article_show"
      * )
      */
     public function showAction()
     {
         return $this->render('article/show.html.twig');
+    }
+
+    /**
+     * @Route("/add", name="article_add")
+     */
+    public function addAction(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'The article was successfully saved in database!');
+
+            return $this->redirectToRoute('article_homepage');
+        }
+
+        return $this->render('article/add.html.twig', ['articleForm' => $form->createView()]);
+    }
+
+    /**
+     * @Route(
+     *     "/update/{id}",
+     *     name="article_update",
+     *     requirements={"id" = "\d+"}
+     * )
+     */
+    public function updateAction(Article $article, Request $request)
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', 'The article was successfully updated in database!');
+
+            return $this->redirectToRoute('article_homepage');
+        }
+
+        return $this->render('article/add.html.twig', [
+            'articleForm' => $form->createView(),
+            'article' => $article,
+        ]);
     }
 }
---
+++ src/AppBundle/Form/ArticleType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'The title of the article',
+                ]
+            ])
+            ->add('headerImage')
+            ->add('author')
+            ->add('content')
+         ;
+    }
+}
