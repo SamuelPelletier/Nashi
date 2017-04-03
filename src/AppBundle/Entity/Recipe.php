9774050fee3d38264c5c5c1fa16a8e061d810729
Sam <samuel.pelletier@hotmail.fr>
2017-04-03 12:14:18 +0200

    Ajout de la connection a Wit.ai

---
+++ src/AppBundle/Entity/Recipe.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table
+ */
+class Recipe
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Recipe
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Recipe
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
---
+++ src/AppBundle/Service/WitImpl.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Service;
+
+/**
+ * Created by PhpStorm.
+ * User: Samuel
+ * Date: 27/03/2017
+ * Time: 11:55
+ */
+use Tgallice\Wit\Client;
+use Tgallice\Wit\MessageApi;
+
+class WitImpl
+{
+    public function connect($language,$sentence){
+        if(strtoupper($language == 'FR')){
+            $token = '43S6T5VI7QEPF36C6BPXOTRSY46JF66G';
+        }else{
+            $token = '5E2VLGDLHTVA6LKFKMOTHX3DY7UB2544';
+        }
+        $client = new Client($token);
+        $response = $client->get('/message', [
+            'q' => $sentence,
+        ]);
+
+// Get the decoded body
+        return json_decode((string) $response->getBody(), true);
+    }
+
+}
